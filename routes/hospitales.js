1e8ccb61d34dfa7b1883a87d3875948ebf1f7cc0
stefangoran61@gmail.com <stefangoran61@gmail.com>
2021-01-18 22:26:33 +0100

    Fin Seccion 13

--- routes/hospitales.js
+++ routes/hospitales.js
@@ -30,11 +30,17 @@ router.post( '/',
 );
 
 router.put( '/:id',
-    [],
+    [
+        validarJWT,
+        check('nombre','El nombre del hospital es necesario').not().isEmpty(),
+        validarCampos
+
+    ],
     actualizarHospital
 );
 
 router.delete( '/:id',
+    validarJWT,
     borrarHospital
 );
 
