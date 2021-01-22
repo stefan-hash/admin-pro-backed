f823bc774c6c6cbc0fb9a16290dc78195d887031
stefangoran61@gmail.com <stefangoran61@gmail.com>
2021-01-22 01:59:42 +0100

    Fin seccion 18

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-
+const Usuario = require('../models/usuario');
 
 
 const validarJWT = (req, res, next) => {
@@ -30,7 +30,85 @@ const validarJWT = (req, res, next) => {
  
 }
 
+const validarADMIN_ROLE = async (req, res, next) => {
+
+    const uid = req.uid;
+
+    try {
+
+        const usuarioDB = await Usuario.findById(uid);
+
+        if( !usuarioDB ){
+            return res.status(400).json({
+                ok:false,
+                msg: 'Usuario no existe'
+            });
+        }
+
+        if ( usuarioDB.role !== 'ADMIN_ROLE' ){
+            return res.status(403).json({
+                ok:false,
+                msg:'No tienes pivilegios'
+            });
+        }
+
+        next();
+       
+    } catch (error) {
+       res.status(500).json({
+
+        ok:false,
+        msg: 'Hable con el administrador'
+
+       }); 
+    }
+
+
+}
+
+const varlidarADMIN_ROLE_o_MismoUsuario = async(req, res, next)  => {
+
+    const uid = req.uid;
+    const id  = req.params.id;
+    
+    try {
+        
+        const usuarioDB = await Usuario.findById(uid);
+
+        if ( !usuarioDB ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Usuario no existe'
+            });
+        }
+
+        if ( usuarioDB.role === 'ADMIN_ROLE' || uid === id ) {
+        
+            next();
+            
+        } else {
+            return res.status(403).json({
+                ok: false,
+                msg: 'No tiene privilegios para hacer eso'
+            });
+        }
+
+        
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+
+}
+
 
 module.exports = {
-    validarJWT
+    validarJWT,
+    validarADMIN_ROLE,
+    varlidarADMIN_ROLE_o_MismoUsuario
 }
